cf343a62ccb63ef1358cb8d55265a40649ecc290
Artem Borodin <54876237+borodun@users.noreply.github.com>
2022-10-17 18:26:28 +0700

    Init backend (#1)
    * init backend project, setup initial spring, add dev and prod profiles, connect database to spring
    
    * add pom.xml to git
    
    Co-authored-by: Nikita <n.omelyanenko@g.nsu.ru>

---
+++ backend/src/main/java/com/rpgstats/controllers/PingController.java
@@ -0,0 +1,14 @@
+package com.rpgstats.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PingController {
+    @RequestMapping(method = RequestMethod.GET, path = "/api/ping")
+    public ResponseEntity<String> getPing() {
+        return ResponseEntity.ok("pong");
+    }
+}
---
+++ backend/src/test/java/com/rpgstats/rpgstats/RpgstatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpgstats.rpgstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgstatsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
